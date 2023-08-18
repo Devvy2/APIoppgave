6362b0f4f927a3cb9d728813929942b3f2678253
Devvy2 <jsorenesh04.kodehode@gmail.com>
2023-08-18 14:41:41 +0200

    Async fetch & display Pokemon data

--- JS/main.js
+++ JS/main.js
@@ -4,19 +4,25 @@
 //  Then parse the response received from API and parse it JSON.
 //  After parsing the JSON data, creates a array called "Pokemon" and store the data fetched into the pokemon array
 
-const fetchPokemon = () => {
-  const url = `https://pokeapi.co/api/v2/pokemon/1`;
-  fetch(url)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      const pokemon = [];
-      pokemon[`name`] = data.name;
-      pokemon[`id`] = data.id;
-      pokemon[`image`] = data.sprites[`front_default`];
-      console.log(pokemon);
-    });
+const fetchPokemon = async () => {
+  try {
+    const url = `https://pokeapi.co/api/v2/pokemon/1`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const nameElement = document.getElementById("pokemonName");
+    const idElement = document.getElementById("pokemonId");
+    const imageElement = document.getElementById("pokemonImage");
+
+    nameElement.textContent = `Name: ${data.name}`;
+    idElement.textContent = `ID: ${data.id}`;
+    imageElement.src = data.sprites.other["official-artwork"].front_default;
+    console.log(imageElement);
+    imageElement.alt = `Image of ${data.name}`;
+  } catch (error) {
+    console.error("error fetching pokemon data", error);
+  }
 };
 
+console.log(fetchPokemon);
 fetchPokemon();
