6c034d4dfd6e7c97672a95685ef057bf911cbd07
Devvy2 <joakimsh04.kodehode@gmaill.com>
2023-08-21 17:09:43 +0200

    Added details button with function to fetch data

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,3 @@
-//  Define a function called fetchPokemon.
-//  then define the URL to fetch data about a Pokémon with ID 1 from the PokeAPI.
-//  and then initialize the a network fetch using the fetch API from URL.
-//  Then parse the response received from API and parse it JSON.
-//  After parsing the JSON data, creates a array called "Pokemon" and store the data fetched into the pokemon array
-
 const fetchPokemon = async () => {
   try {
     const container = document.getElementById("pokemonContainer");
@@ -41,14 +35,70 @@ async function renderPokemon(container, data) {
     .map((type) => type.type.name)
     .join(", ")}`;
 
+  const pokemonDetails = document.createElement("button");
+  pokemonDetails.textContent = "Click for details";
+
+  pokemonDetails.addEventListener("click", () => {
+    showPokemonDetailsModal(data);
+  });
+
   pokemonDiv.appendChild(imageElement);
   pokemonDiv.appendChild(nameElement);
   pokemonDiv.appendChild(idElement);
   pokemonDiv.appendChild(typeElement);
-
-  console.log(typeElement);
+  pokemonDiv.appendChild(pokemonDetails);
 
   container.appendChild(pokemonDiv);
+}
+
+function showPokemonDetailsModal(data) {
+  const modal = document.createElement("div");
+  modal.className = "modal";
+
+  const closeModalButton = document.createElement("button");
+  closeModalButton.textContent = "Close";
+  closeModalButton.addEventListener("click", () => {
+    document.body.removeChild(modal);
+  });
+
+  const detailsElement = document.createElement("div");
+  detailsElement.className = "pokemon-details";
+
+  detailsElement.innerHTML = `
+    <h2>${data.name.toUpperCase()}</h2>
+    <img src="${
+      data.sprites.other["official-artwork"].front_default
+    }" alt="Image of ${data.name}">
+    <p id="pokemon-id">ID: ${data.id}</p>
+    <p id="pokemon-hp">HP: ${
+      data.stats.find((stat) => stat.stat.name === "hp").base_stat
+    }</p>
+    <p id="pokemon-attack">Attack: ${
+      data.stats.find((stat) => stat.stat.name === "attack").base_stat
+    }</p>
+    </p>
+     <p id="pokemon-special-attack">Special-attack: ${
+       data.stats.find((stat) => stat.stat.name === "special-attack").base_stat
+     }</p>
+    <p id="pokemon-defense">Defense: ${
+      data.stats.find((stat) => stat.stat.name === "defense").base_stat
+    }
+      <p id="pokemon-special-defense">Special-defense: ${
+        data.stats.find((stat) => stat.stat.name === "special-defense")
+          .base_stat
+      }</p>
+      <p id="pokemon-speed">Speed: ${
+        data.stats.find((stat) => stat.stat.name === "speed").base_stat
+      }</p>
+    <p>Type: ${data.types.map((type) => type.type.name).join(", ")}</p>
+  `;
+
+  // Create and append details about the Pokemon to detailsElement
+
+  detailsElement.appendChild(closeModalButton);
+  modal.appendChild(detailsElement);
+
+  document.body.appendChild(modal);
 }
 
 fetchPokemon();
